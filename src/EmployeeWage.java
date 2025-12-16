033419ad87cce08acc88fd950242f5630cd6188c
rrajput7461 <rohitr_ug_22@mech.nits.ac.in>
2025-12-16 13:36:27 +0530

    UC1 Employee Attendence

---
+++ src/EmployeeWage.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class EmployeeWage {
+
+    public static void checkAttendance() {
+        int IS_PRESENT = 1;
+        Random random = new Random();
+        int empCheck = random.nextInt(2);
+
+        if (empCheck == IS_PRESENT)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
-import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        int IS_PRESENT = 1;
-        Random random = new Random();
-        int empCheck = random.nextInt(2);
-
-        if (empCheck == IS_PRESENT)
-            System.out.println("Employee is Present");
-        else
-            System.out.println("Employee is Absent");
+        System.out.println("Welcome to Employee Wage Computation Program");
+        EmployeeWage.checkAttendance();
     }
 }
